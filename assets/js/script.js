a0631426ad8721a3e6b42728e6e5a39c0ee2668e
Blommis <blomquist799@gmail.com>
2025-04-01 11:03:32 +0200

    created startGame function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-function startGame(){}
+function startGame(){
+let name = document.getElementById("playerName").value; 
+    if(name.trim() === ""){
+        alert("You have to write in your username to play!");
+        return;
+    }
+    // To remove intro section and add greeting instead
+    let introSection = document.getElementById("intro");
+    introSection.innerHTML = `<h2 id="greeting">Good luck, ${name}!</h2>`;
+    
+    document.getElementById("game").classList.remove("hidden");
+}
 
 function createGameBoard(){}
-
 function flipCard(){}
 
 function checkMate(){}
